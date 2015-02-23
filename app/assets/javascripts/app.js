8c53852fb753d16fc4910732322f61c4c033c5ad
Juan Antonio Galisteo <jgalisteo@nosolosoftware.biz>
2015-02-23 13:33:44 +0100

    Primer controlador.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,20 @@
 (function(){
   var app = angular.module('mallesti', []);
+
+  app.controller('CustomerController', function(){
+    this.customer = customer;
+  });
+
+  var customer = {
+    name: "Melyna Rau",
+    address: "7220 Jakubowski Freeway",
+    town: "Lake Juliofurt",
+    city: "Hilbertside",
+    country: "Republic of Korea",
+    zip_code: "43650-0519",
+    phone: "1-206-502-0584",
+    cif: "00000001R",
+    email: "user1@email.com"
+  }
+
 })();
