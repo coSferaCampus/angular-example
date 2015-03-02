bc67f8772ed76fc96b8a9cfb3b97a411c9e22c79
Juan Antonio Galisteo Prieto <jgalisteo@nosolosoftware.biz>
2015-03-02 20:29:25 +0100

    Login refactorizado.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,14 +2,15 @@
   // App module
   var app = angular.module(
     'mallesti',
-    ['ui.router', 'templates', 'mallesti-customer', 'mallesti-project', 'LocalStorageModule']
+    ['ui.router', 'templates', 'mallesti-customer', 'mallesti-project', 'mallesti-auth']
   );
 
+  // Esto pasa el token y el email en cada petición que hagamos a la API
   app.factory("httpInterceptor", ['AuthService', function(AuthService) {
     return {
       request: function(config) {
-        config.headers['X-User-Email'] = AuthService.currentUser().email;
-        config.headers['X-User-Token'] = AuthService.currentUser().authentication_token;
+        config.headers['X-User-Email'] = AuthService.currentUserEmail();
+        config.headers['X-User-Token'] = AuthService.currentUserToken();
         return config;
       }
     };
@@ -45,46 +46,4 @@
         templateUrl: "customer-projects.html"
       });
   });
-
-  // login
-  app.controller(
-    "LoginController",
-    ['$http', '$state', 'AuthService', function($http, $state, AuthService){
-      var scope = this;
-      scope.session = {};
-      scope.error = false
-
-      scope.login = function() {
-        $http.post(
-          "http://localhost:4000/session.json",
-          {email: scope.session.email, password: scope.session.password}
-        )
-          .success(function(data) {
-            AuthService.saveUser(data.user);
-            $state.go("home.info");
-          })
-          .error(function(data) {
-            scope.session = {};
-            scope.error = true;
-          })
-      };
-    }]
-  );
-
-  app.factory("AuthService", ['localStorageService', function(localStorageService) {
-    var currentUser = localStorageService.get("user");
-    return {
-      currentUser: function() {
-        return currentUser;
-      },
-      saveUser: function(user) {
-        localStorageService.set("user", user);
-        currentUser = user;
-      },
-      destroyUser: function() {
-        localStorageService.set("user", null);
-        currentUser = null;
-      }
-    };
-  }]);
 })();
